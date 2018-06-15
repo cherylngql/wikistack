f5394db1c5064d2c030496ca9ac4ef4f6055830f
Cheryl Ng <cherylngql@gmail.com>
2018-06-15 13:46:47 -0400

    finished schema

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const { db } = require('./models');
 const routes = require('./routes/mainRoute');
 
 const app = express();
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -17,7 +22,12 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 const PORT = 3000;
+console.log('this is db: ', db.User);
+const init = async () => {
+  await db.sync({force: true});
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+init();
